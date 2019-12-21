6f1138480c548df29e13f3bb9c62a71192f1adb4
PawelM-code <maciejewski.pawel@o2.pl>
2019-12-22 00:04:10 +0100

    JDP191201-28 - CartController - initial endpoints implementation - add Cart and CartDto class - add empty User class

---
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -0,0 +1,19 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cart {
+    private Long id;
+    private List<Product> products = new ArrayList<>();
+    private User user;
+}
