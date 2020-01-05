8c2ba191d74d4da72fb6483a29b12fa5cfd4012a
kaszakinga90 <kaszakinga90@gmail.com>
2020-01-05 19:23:33 +0100

    JDP191201-37

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -1,17 +1,43 @@
 package com.kodilla.ecommercee.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @NotNull
+    @Column(name = "product_id")
     private Long id;
+
+    @Column(name = "product_name")
+    @NotNull
     private String productName;
+
+    @Column(name = "product_type")
     private String productType;
+
     @ManyToOne
     @JoinColumn(name = "group_id")
     public Group group;
+
+
+    @ManyToMany(cascade = CascadeType.ALL, mappedBy = "products")
+    private List<Cart> carts = new ArrayList<>();
+
+
+
 }
