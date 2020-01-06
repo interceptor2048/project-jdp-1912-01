2b752cf58f7b924f297b90b5c6a096af495b1df8
DariuszKny <nasz_email_do_github>
2020-01-06 15:39:38 +0100

    # JDP191201- 40 -Added UserRepository - No need to change User.Class

--- src/main/java/com/kodilla/ecommercee/UserRepository.java
+++ src/main/java/com/kodilla/ecommercee/UserRepository.java
@@ -1,12 +1,13 @@
 package com.kodilla.ecommercee;
 
 import com.kodilla.ecommercee.domain.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 
 import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     @Override
     List<User> findAll();
