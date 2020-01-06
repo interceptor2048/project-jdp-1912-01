5543ea69734ca40d93911368ba54bf21142444ae
kaszakinga90 <kaszakinga90@gmail.com>
2020-01-06 13:19:06 +0100

    JDP191201-37 commit

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -5,15 +5,32 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "carts")
 public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "cart_id")
+    @NotNull
     private Long id;
+    //private User user;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "join_products_cart",
+            joinColumns = {@JoinColumn(name = "cart_id", referencedColumnName = "cart_id")},
+            inverseJoinColumns = {@JoinColumn(name = "product_id", referencedColumnName = "product_id")}
+    )
     private List<Product> products = new ArrayList<>();
-    private User user;
+
+
 }
--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -10,22 +10,31 @@ import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Getter
 @Setter
+@Table(name = "products")
 public class Product {
     @Id
     @Column(name = "product_id")
     @NotNull
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(name = "product_name")
+    @NotNull
     private String productName;
+
+    @Column(name = "product_type")
     private String productType;
+
     @ManyToOne
     @JoinColumn(name = "group_id")
     public Group group;
+
     @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(
             name = "join_orders_product",
@@ -33,4 +42,7 @@ public class Product {
             inverseJoinColumns = {@JoinColumn(name = "order_id", referencedColumnName = "order_id")}
     )
     private List<Order> orders = new ArrayList<>();
+
+    @ManyToMany(cascade = CascadeType.ALL, mappedBy = "products")
+    private List<Cart> carts = new ArrayList<>();
 }
