48db36d5e40db73eadf4e373bcd715839ab5fc7b
kaszakinga90 <kaszakinga90@gmail.com>
2020-01-18 12:13:36 +0100

    JDP191201-49

--- src/main/java/com/kodilla/ecommercee/controller/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/controller/OrderController.java
@@ -1,39 +1,47 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.domain.OrderNotFoundException;
+import com.kodilla.ecommercee.domain.UserNotFoundException;
 import com.kodilla.ecommercee.domain.dto.OrderDto;
+import com.kodilla.ecommercee.mapper.OrderMapper;
+import com.kodilla.ecommercee.service.DbOrderService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/v1/order")
 public class OrderController {
+    private final DbOrderService dbOrderService;
+    private final OrderMapper orderMapper;
 
     @GetMapping(value = "getOrders")
     public List<OrderDto> getOrders() {
-        return new ArrayList<>();
+        return orderMapper.mapToOrderDtoList(dbOrderService.getAllOrders());
     }
 
     @GetMapping(value = "getOrder")
-    public OrderDto getOrder(@RequestParam Long orderId) {
-        return new OrderDto(1L, "orderName");
+    public OrderDto getOrder(@RequestParam Long orderId) throws UserNotFoundException {
+        return orderMapper.mapToOrderDto(dbOrderService.getOrder(orderId).orElseThrow(OrderNotFoundException::new));
     }
 
     @DeleteMapping(value = "deleteOrder")
     public void deleteOrder(@RequestParam Long orderId) {
-
+        dbOrderService.deleteOrder(orderId);
     }
 
     @PutMapping(value = "updateOrder")
-    public OrderDto updateOrder(@RequestBody OrderDto orderDto) {
-        return new OrderDto(1L, "orderName");
+    public OrderDto updateOrder(@RequestBody OrderDto orderDto) throws OrderNotFoundException, UserNotFoundException {
+        return orderMapper.mapToOrderDto(dbOrderService.updateOrder(
+                orderDto.getId(),
+                orderMapper.mapToOrder(orderDto)));
     }
 
     @PostMapping(value = "createOrder")
-    public void createOrder(@RequestBody OrderDto orderDto) {
-
+    public void addOrder(@RequestBody OrderDto orderDto) throws UserNotFoundException {
+        dbOrderService.createOrder(orderMapper.mapToOrder(orderDto));
     }
-
 }
--- src/main/java/com/kodilla/ecommercee/domain/dto/OrderDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/dto/OrderDto.java
@@ -10,4 +10,5 @@ import lombok.NoArgsConstructor;
 public class OrderDto {
     private Long id;
     private String orderName;
+    UserDto userDto;
 }
